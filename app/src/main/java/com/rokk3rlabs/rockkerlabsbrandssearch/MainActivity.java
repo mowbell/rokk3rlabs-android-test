b0df7d6be473345e9a9cb96244c79a220433364b
Mauricio Bello <mow_bell@hotmail.com>
2016-04-06 17:18:54 -0500

    Searchview en action bar Probando querys

--- app/src/main/java/com/rokk3rlabs/rockkerlabsbrandssearch/MainActivity.java
+++ app/src/main/java/com/rokk3rlabs/rockkerlabsbrandssearch/MainActivity.java
@@ -1,22 +1,33 @@
 package com.rokk3rlabs.rockkerlabsbrandssearch;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.rokk3rlabs.rockkerlabsbrandssearch.database.AppDatabaseTable;
 
+
+public class MainActivity extends AppCompatActivity {
+    protected AppDatabaseTable db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        db=new AppDatabaseTable(this);
+        db.mDatabaseOpenHelper.getReadableDatabase();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,13 +37,77 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.v("MOW", "Searching " + query);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Assumes current activity is the searchable activity
+        //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default*/
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.v("MOW", "onQueryTextSubmit " + query);
+                doSearchQuery(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.v("MOW", "onQueryTextChange " + newText);
+                return false;
+            }
+        });
         return true;
+    }
+
+    private void doSearchQuery(String query) {
+        Cursor mCursor=db.getWord(query);
+
+        Log.v("MOW","Cursor data "+mCursor.getCount());
+        if (null == mCursor) {
+			    /*
+			     * Insert code here to handle the error. Be sure not to use the cursor! You may want to
+			     * call android.util.Log.e() to log this error.
+			     *
+			     */
+            // If the Cursor is empty, the provider found no matches
+        } else if (mCursor.getCount() < 1) {
+
+			    /*
+			     * Insert code here to notify the user that the search was unsuccessful. This isn't necessarily
+			     * an error. You may want to offer the user the option to insert a new row, or re-type the
+			     * search term.
+			     */
+
+        } else {
+            // Insert code here to do something with the results
+            //mCursor.moveToFirst();
+            while (mCursor.moveToNext()) {
+                for(int i=0; i<mCursor.getColumnNames().length;i++){
+                    Log.v("MOW",mCursor.getColumnNames()[i]);
+                }
+
+                String brand=mCursor.getString(mCursor.getColumnIndex("resultado"));
+                Log.v("MOW","BRAND FOUND "+brand);
+
+            }
+
+
+        }
     }
 
     @Override
@@ -43,9 +118,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/rokk3rlabs/rockkerlabsbrandssearch/database/AppDatabaseTable.java
@@ -0,0 +1,121 @@
+package com.rokk3rlabs.rockkerlabsbrandssearch.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.util.Log;
+
+import java.util.HashMap;
+
+/**
+ * Created by Mauricio on 06/04/2016.
+ */
+public class AppDatabaseTable {
+    private static final String DATABASE_NAME = "ROKK3R_LABS_BRANDS";
+
+    private static final String FTS_VIRTUAL_TABLE_BRAND = "FTS_BRAND";
+
+    public static final String COL_NAME = "NAME";
+
+    private static final String FTS_VIRTUAL_TABLE_CLOTHING = "FTS_CLOTHING_TYPE";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public final DatabaseOpenHelper mDatabaseOpenHelper;
+
+    public AppDatabaseTable(Context context) {
+        this.mDatabaseOpenHelper = new DatabaseOpenHelper(context);
+    }
+
+    public static class DatabaseOpenHelper extends SQLiteOpenHelper {
+
+        private static final String FTS_TABLE_BRAND_CREATE =
+                "CREATE TABLE " + FTS_VIRTUAL_TABLE_BRAND +
+                        " (" +
+                        COL_NAME + ")";
+
+        private static final String FTS_TABLE_CLOTHING_TYPE_CREATE =
+                "CREATE TABLE " + FTS_VIRTUAL_TABLE_CLOTHING +
+                        " (" +
+                        COL_NAME + ")";
+
+        private final Context mHelperContext;
+        private SQLiteDatabase mDatabase;
+
+        public DatabaseOpenHelper(Context context) {
+
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+            mHelperContext = context;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            mDatabase = db;
+            mDatabase.execSQL(FTS_TABLE_BRAND_CREATE);
+
+
+            mDatabase.execSQL("INSERT INTO "+FTS_VIRTUAL_TABLE_BRAND+ " ("+COL_NAME+") VALUES ('Gap'),('Banana Republic'),('Boss'),('Hugo Boss'),('Taylor'),('Rebecca Taylor')");
+
+            /*ContentValues initialValues = new ContentValues();
+            initialValues.put(COL_NAME, "GAP");
+            mDatabase.insert(FTS_TABLE_BRAND_CREATE, null, initialValues);*/
+
+            mDatabase.execSQL(FTS_TABLE_CLOTHING_TYPE_CREATE);
+
+            mDatabase.execSQL("INSERT INTO "+FTS_VIRTUAL_TABLE_CLOTHING+ " ("+COL_NAME+") VALUES ('Denim'),('Pants'),('Sweaters'),('Skirts'),('Dresses')");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + FTS_VIRTUAL_TABLE_BRAND);
+            db.execSQL("DROP TABLE IF EXISTS " + FTS_VIRTUAL_TABLE_CLOTHING);
+        }
+
+
+    }
+
+    public Cursor getAllBrands() {
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(FTS_VIRTUAL_TABLE_BRAND);
+
+        Log.v("MOW Query", builder.buildQuery(null, null, null, null, COL_NAME, null));
+        Cursor cursor = builder.query(mDatabaseOpenHelper.getReadableDatabase(),
+                null, null, null, null, null, COL_NAME);
+
+        return cursor;
+    }
+
+    public Cursor getWord(String brand) {
+        //String selection = COL_NAME + " LIKE ? OR "+KEY_DEFINITION + " LIKE ? ";
+        /*String selection = COL_NAME + " LIKE ? ";
+        String[] selectionArgs = new String[] {"%"+brand+"%"};*/
+
+        /*String selection = "instr(?,"+COL_NAME+") AS resultado";
+        String[] selectionArgs = new String[] {brand};
+        String[] columns = new String[]{"resultado"};
+
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(FTS_VIRTUAL_TABLE_BRAND);
+        Log.v("MOW Query", builder.buildQuery(columns, selection, selectionArgs, null, null, null, null));
+        Cursor cursor = builder.query(mDatabaseOpenHelper.getReadableDatabase(),
+                columns, selection, selectionArgs, null, null, null);*/
+
+
+        String[] columns = new String[]{COL_NAME, "length(trim(replace('"+brand+"',"+COL_NAME+",''))) AS resultado"};
+
+
+
+        //String selection = "length(trim(replace('"+brand+"',"+COL_NAME+",'')))=0";
+
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(FTS_VIRTUAL_TABLE_BRAND);
+        Log.v("MOW Query", builder.buildQuery(columns, null, null, null, null, null, null));
+        Cursor cursor = builder.query(mDatabaseOpenHelper.getReadableDatabase(),
+                columns, null, null, null, null, null);
+        return cursor;
+    }
+
+}
